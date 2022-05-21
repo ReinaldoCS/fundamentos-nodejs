d9db9da2f20d4f0f6c16e8196b15dfa2c3487c52
reinaldocs <santosdoscorreiareinaldo@gmail.com>
2022-05-21 19:05:58 -0300

    Utilizando os métodos HTTP

--- src/index.js
+++ src/index.js
@@ -2,9 +2,33 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello world Ignite! - Fundamentos NodeJS' })
-})
+/**
+ * GET - Buscar uma informação dentro do servidor
+ * POST - Inserir uma informação no servidor
+ * PUT - Alterar uma informação no servidor
+ * PATCH - Alterar uma informação específica
+ * DELETE - Deletar uma informação no servidor
+ */
+
+app.get('/courses', (request, response) => {
+  return response.json(['curso 1', 'curso 2', 'curso 3'])
+});
+
+app.post('/courses', (request, response) => {
+  return response.json(['curso 1', 'curso 2', 'curso 3', 'curso 4'])
+});
+
+app.put('/courses/:id', (request, response) => {
+  return response.json(['curso 6', 'curso 2', 'curso 3', 'curso 4'])
+});
+
+app.patch('/courses/:id', (request, response) => {
+  return response.json(['curso 1', 'curso 7', 'curso 3', 'curso 4'])
+});
+
+app.delete('/courses/:id', (request, response) => {
+  return response.json(['curso 1', 'curso 2', 'curso 4'])
+});
 
 app.listen(3333, () => {
   console.log('servidor rodando na porta: 3333');
