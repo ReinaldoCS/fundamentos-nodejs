d88be775dc692de3937cc368381eb567330d7e74
reinaldocs <santosdoscorreiareinaldo@gmail.com>
2022-05-21 19:22:27 -0300

    Tipos de parâmetros

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+// Função para poder trabahar com JSON nas rodas HTTP
+app.use(express.json());
+
 /**
  * GET - Buscar uma informação dentro do servidor
  * POST - Inserir uma informação no servidor
@@ -10,15 +13,42 @@ const app = express();
  * DELETE - Deletar uma informação no servidor
  */
 
+/**
+ * Tipos de parâmetros
+ * 
+ * Route Params => Identificar um recurso editar/deletar/buscar
+ * Query Params => Paginação / Filtro
+ * Body Parms => Os objetos inserção/alteração (JSON)
+ */
+
 app.get('/courses', (request, response) => {
+  /**
+   * O Route Params não é obrigatório para esse tipo de rota,
+   * caso não passe não é apresentado um erro
+   */
+
+  const query = request.query;
+  console.log(query);
+
   return response.json(['curso 1', 'curso 2', 'curso 3'])
 });
 
 app.post('/courses', (request, response) => {
+  const body = request.body;
+  console.log(body);
+
   return response.json(['curso 1', 'curso 2', 'curso 3', 'curso 4'])
 });
 
 app.put('/courses/:id', (request, response) => {
+  /**
+   * O Route Params é obrigatório para esse tipo de rota,
+   * caso não passe é disparado um erro
+   */
+
+  const { id } = request.params;
+  console.log(id);
+
   return response.json(['curso 6', 'curso 2', 'curso 3', 'curso 4'])
 });
 
